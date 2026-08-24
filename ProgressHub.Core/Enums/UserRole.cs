98339dd6848b43e0bd5bbe0f11d5a5ab46567e2c
manolachelucian <lucius.manolache@gmail.com>
2026-08-24 20:00:03 +0200

    + Updated Core

---
+++ ProgressHub.Core/Enums/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressHub.Core.Enums
+{
+    public enum UserRole
+    {
+        Admin, // 0
+        Client, // 1
+        Coach // 2 
+
+    }
+}
--- ProgressHub.Core/Models/User.cs
+++ ProgressHub.Core/Models/User.cs
@@ -1,17 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ProgressHub.Core.Enums;
+
 
 namespace ProgressHub.Core.Models
 {
     public class User
     {
-        public int Id { get; private set; } 
-        private string Name { get; set; }
 
-        private DateTime BirthDate { get; set; }
+        //Basic profile informations
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public UserRole UserRole { get; set; } = UserRole.Client;
+       
+        //Physic parameters for calculation of BMI and etc...
+
+        public int HeightInCm { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+
+
+
+        // Goals from Coach
+        public int TargetCalories { get; set; }
+        public int TargetProteinGrams { get; set; }
+        public int TargetCarbsGrams { get; set; }
+        public int TargetFatsGrams { get; set; }
+
+        // Propojení na záznamy (vazba 1:N)
+        //public List<DailyLog> DailyLogs { get; set; } = new();
+
+
+
+        public int GetAge()
+        {
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+            var age = today.Year - DateOfBirth.Year;
+
+            if (DateOfBirth > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+        public string GetFullName() => $"Fullname: {FirstName} {LastName}".Trim();
+
+
 
     }
 }
