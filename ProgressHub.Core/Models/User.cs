5cdfd4a32dfd9a6f51c28c70fcb340af2fc6b0dd
manolachelucian <lucius.manolache@gmail.com>
2026-08-24 19:21:45 +0200

    Updated Structure

---
+++ ProgressHub.Core/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressHub.Core.Models
+{
+    public class User
+    {
+        public int Id { get; private set; } 
+        private string Name { get; set; }
+
+        private DateTime BirthDate { get; set; }
+
+    }
+}
